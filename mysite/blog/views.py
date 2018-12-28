505ba1f2b8fee1e62375fba91b0a5d4f4112db2f
uzzal71 <uzzalroy.acm@gmail.com>
2018-12-28 08:26:31 +0600

    Create Template and Static

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils import timezone
+
+
+posts = [
+    {
+        'author': 'Nasir Uddin',
+        'title': 'Blog post one',
+        'content': 'First blog post content',
+        'date_posted': timezone.now()
+    },
+    {
+        'author': 'Juwel Rana',
+        'title': 'Blog post two',
+        'content': 'Second blog post content',
+        'date_posted': timezone.now()
+    }
+]
 
 def home(request):
-    return HttpResponse('<h2>Blog Home</h2>')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h2>Blog About</h2>')
+    return render(request, 'blog/about.html', {'title': 'About'})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,4 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
