886cf94d26e3b634c2f457825e6cf8b8d4455bfa
uzzal71 <uzzalroy.acm@gmail.com>
2018-12-28 18:23:04 +0600

    User Login and logout created

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,3 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
