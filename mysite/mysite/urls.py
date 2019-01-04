24244cf8f6f9a5062286f2db9f7ce12c6ed67cd3
uzzal71 <uzzalroy.acm@gmail.com>
2019-01-04 13:46:12 +0600

    Email and password reset

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,6 +26,17 @@ urlpatterns = [
     path('profile/', user_view.profile, name='profile'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', auth_view.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_view.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_view.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('', include('blog.urls')),
 ]
 
